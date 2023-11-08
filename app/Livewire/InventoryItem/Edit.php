62c20fd3369889994b7869534eaf0812bd7b25c3
fassadicon <fa.sadicon@gmail.com>
2023-11-09 00:36:55 +0800

    base files

---
+++ app/Livewire/InventoryItem/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\InventoryItem;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.inventory-item.edit');
+    }
+}
