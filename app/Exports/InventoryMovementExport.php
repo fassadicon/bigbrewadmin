88e5d96f9779d977f9c0344e4c8b6fbd6075e667
fassadicon <fa.sadicon@gmail.com>
2023-11-29 23:37:51 +0800

    revise column arrangement

--- app/Exports/InventoryMovementExport.php
+++ app/Exports/InventoryMovementExport.php
@@ -4,6 +4,7 @@ namespace App\Exports;
 
 use App\Models\InventoryLog;
 use Illuminate\Support\Carbon;
+use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Concerns\WithMapping;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -14,11 +15,16 @@ use Maatwebsite\Excel\Concerns\WithColumnWidths;
 class InventoryMovementExport implements FromCollection, WithMapping, WithHeadings, ShouldAutoSize
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
+
+    public $count;
+
     public function collection()
     {
-        return InventoryLog::all();
+        $inventoryLogs = InventoryLog::all();
+        $this->count = $inventoryLogs->count();
+        return $inventoryLogs;
     }
 
     public function map($inventoryLog): array
@@ -27,16 +33,36 @@ class InventoryMovementExport implements FromCollection, WithMapping, WithHeadin
             $inventoryLog->id,
             $inventoryLog->inventory_item_id,
             $inventoryLog->inventoryItem->name,
-            $inventoryLog->supplier,
             $inventoryLog->user->name,
             $inventoryLog->type,
             $inventoryLog->amount,
             $inventoryLog->old_stock,
             $inventoryLog->new_stock,
             $inventoryLog->remarks,
+            $inventoryLog->supplier,
             Carbon::parse($inventoryLog->created_at)->format('M n, Y h:i A')
         ];
     }
+
+    // public function registerEvents(): array
+    // {
+    //     return [
+    //         AfterSheet::class => function (AfterSheet $event) {
+    //             // Get the total number of rows
+    //             $event->sheet->getStyle('A' . 2)->getFont()->setBold(true);
+    //             // $totalRows = 10;
+
+    //             // // Loop through each row and apply conditional formatting
+    //             // for ($row = 2; $row <= $totalRows + 1; $row++) {
+    //             //     $cellValue = $event->sheet->getCell('A' . $row)->getValue();
+    //             //     if ($cellValue == "1") {
+    //             //         // Apply bold font to cell A in the current row
+    //             //         $event->sheet->getStyle('A' . $row)->getFont()->setBold(true);
+    //             //     }
+    //             // }
+    //         },
+    //     ];
+    // }
 
     public function headings(): array
     {
@@ -44,13 +70,13 @@ class InventoryMovementExport implements FromCollection, WithMapping, WithHeadin
             'INVENTORY LOG ID',
             'INVENTORY ITEM ID',
             'INVENTORY ITEM NAME',
-            'SUPPLIER',
             'ACTION BY',
             'TYPE',
             'AMOUNT',
             'OLD STOCK',
             'NEW STOCK',
             'REMARKS',
+            'SUPPLIER',
             'DATE',
         ];
     }
