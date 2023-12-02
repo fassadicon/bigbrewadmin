840e068e44eb51b0d9f6a72acb45fe4daede7b6c
fassadicon <fa.sadicon@gmail.com>
2023-12-02 21:16:46 +0800

    Sample Seeder

--- database/seeders/PurchaseOrderSeeder.php
+++ database/seeders/PurchaseOrderSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\InventoryItem;
+use App\Models\PurchaseOrder;
+use App\Models\PurchaseOrderItem;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +15,31 @@ class PurchaseOrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $inventoryItem = InventoryItem::where('id', 1)->first();
+        $quantity = 5;
+        $purchaseOrderItems = [
+            [
+                'user_id' => 1,
+                'inventory_item_id' => $inventoryItem->id,
+                'quantity' => $quantity,
+                'unit_measurement' => $inventoryItem->measurement,
+                'unit_price' => $inventoryItem->unit_price,
+                'amount' => $quantity * $inventoryItem->unit_price,
+                'description' => '',
+                'status' => 1 // 1 - Pending, 2 - Incomplete, 3 - Completed
+            ]
+        ];
+
+        $purchaseOrder = PurchaseOrder::create([
+            'user_id' => 1,
+            'supplier_id' => 1, // 1 - BigBrew, 2 and others new suppliers
+            'total_amount',
+            'status' => 1 // 1 - Pending, 2 - Incomplete, 3 - Completed
+        ]);
+
+        foreach ($purchaseOrderItems as $purchaseOrderItem) {
+            $purchaseOrderItem['purchase_order_id'] = $purchaseOrder->id;
+            PurchaseOrderItem::create($purchaseOrderItem);
+        }
     }
 }
