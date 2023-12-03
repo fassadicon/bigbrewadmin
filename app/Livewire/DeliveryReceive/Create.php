5375d7e641a0690149699b95e090bf7ed84fe3e0
fassadicon <fa.sadicon@gmail.com>
2023-12-03 17:57:12 +0800

    base create DR

--- app/Livewire/DeliveryReceive/Create.php
+++ app/Livewire/DeliveryReceive/Create.php
@@ -2,22 +2,138 @@
 
 namespace App\Livewire\DeliveryReceive;
 
-use App\Models\PurchaseOrder;
 use Livewire\Component;
+use App\Models\InventoryLog;
+use App\Models\InventoryItem;
+use App\Models\PurchaseOrder;
+use App\Models\DeliveryReceive;
+use App\Models\DeliveryReceiveItem;
 
 class Create extends Component
 {
     public $selectedPurchaseOrder;
     public $purchaseOrders;
+    public $deliveryReceiveItems;
+    public $inventoryItems;
 
-    public function mount() {
+    public function mount()
+    {
         $this->purchaseOrders = PurchaseOrder::whereIn('status', [1, 2])->get();
+        $this->inventoryItems = InventoryItem::all();
     }
 
-    public function selectPurchaseOrder($purchaseOrderId) {
+    public function updateReceived($key, $value)
+    {
+        $pending = $this->deliveryReceiveItems[$key]['expected_quantity'] - $value;
+        $this->deliveryReceiveItems[$key]['pending'] = $pending;
+        $this->deliveryReceiveItems[$key]['quantity'] = $value;
+    }
+
+    public function store()
+    {
+        $deliveryReceiveItemsCreated = [];
+        $totalAmount = 0;
+        $incomplete = false;
+        foreach ($this->deliveryReceiveItems as $deliveryReceiveItem) {
+            $status = 1;
+            if ($deliveryReceiveItem['quantity'] == 0) {
+                $status = 1;
+            } else {
+                if ($deliveryReceiveItem['quantity'] < $deliveryReceiveItem['expected_quantity']) {
+                    $status = 2;
+                    $incomplete = true;
+                } else if ($deliveryReceiveItem['quantity'] >= $deliveryReceiveItem['expected_quantity']) {
+                    $status = 3;
+                }
+            }
+
+
+            $deliveryReceiveItemsCreated[] =
+                [
+                    'purchase_order_item_id' => $deliveryReceiveItem['purchase_order_item_id'],
+                    'user_id' => auth()->id(),
+                    'inventory_item_id' => $deliveryReceiveItem['inventory_item_id'],
+                    'quantity' => $deliveryReceiveItem['quantity'],
+                    'pending' => $deliveryReceiveItem['pending'],
+                    'unit_measurement' => $deliveryReceiveItem['unit_measurement'],
+                    'unit_price' => $deliveryReceiveItem['unit_price'],
+                    'amount' => $deliveryReceiveItem['quantity'] * $deliveryReceiveItem['unit_price'],
+                    'description' => $deliveryReceiveItem['description'],
+                    'status' => $status // 1 - Pending, 2 - Incomplete, 3 - Completed
+                ];
+            $totalAmount += $deliveryReceiveItem['quantity'] * $deliveryReceiveItem['unit_price'];
+        }
+
+        $deliveryReceive = DeliveryReceive::create([
+            'user_id' => 1,
+            'purchase_order_id' => $this->selectedPurchaseOrder->id,
+            'total_amount' => $totalAmount,
+            'status' => $incomplete ? 2 : 3 // 1 - Pending, 2 - Incomplete, 3 - Completed
+        ]);
+
+        foreach ($deliveryReceiveItemsCreated as $deliveryReceiveItem) {
+            $deliveryReceiveItem['delivery_receive_id'] = $deliveryReceive->id;
+            DeliveryReceiveItem::create($deliveryReceiveItem);
+        }
+
+        foreach ($this->selectedPurchaseOrder->purchaseOrderItems as $key => $purchaseOrderItem) {
+            $purchaseOrderItem->update([
+                'status' => $deliveryReceiveItemsCreated[$key]['status']
+            ]);
+        }
+        $this->selectedPurchaseOrder->update([
+            'status' => $incomplete ? 2 : 3
+        ]);
+
+        foreach ($deliveryReceive->deliveryReceiveItems as $deliveryReceiveItem) {
+            $addedStock = $deliveryReceiveItem->quantity;
+            $remainingStocks = $deliveryReceiveItem->inventoryitem->remaining_stocks;
+            $newStocks = $remainingStocks + $addedStock;
+            $deliveryReceiveItem->inventoryItem->update([
+                'remaining_stocks' => $newStocks
+            ]);
+
+            InventoryLog::create([
+                'inventory_item_id' => $deliveryReceiveItem->inventoryItem->id,
+                'user_id' => 1,
+                'type' => 'out',
+                'amount' => $addedStock,
+                'old_stock' => $remainingStocks,
+                'new_stock' => $newStocks,
+                'remarks' => "Added stock from PO: " . $deliveryReceive->purchaseOrder->id .
+                    "-" .
+                    $deliveryReceiveItem->purchaseOrderItem->id .
+                    " DR: " .
+                    $deliveryReceive->id .
+                    "-" .
+                    $deliveryReceiveItem->id .
+                    " for " .
+                    $deliveryReceiveItem->inventoryItem->name
+            ]);
+        }
+    }
+
+    public function selectPurchaseOrder($purchaseOrderId)
+    {
         $this->selectedPurchaseOrder = PurchaseOrder::with('purchaseOrderItems', 'supplier', 'user')
-        ->where('id', $purchaseOrderId)
-        ->first();
+            ->where('id', $purchaseOrderId)
+            ->first();
+        foreach ($this->selectedPurchaseOrder->purchaseOrderItems as $purchaseOrderItem) {
+            $this->deliveryReceiveItems[] =  [
+                'purchase_order_item_id' => $purchaseOrderItem->id,
+                'inventory_item_id' => $purchaseOrderItem->inventory_item_id,
+                'expected_quantity' => $purchaseOrderItem->quantity,
+                'quantity' => 0,
+                'pending' => $purchaseOrderItem->quantity,
+                'unit_measurement' => $purchaseOrderItem->unit_measurement,
+                'unit_price' => $purchaseOrderItem->unit_price,
+                'amount' => $purchaseOrderItem->amount,
+                'description' => $purchaseOrderItem->description
+            ];
+        }
+
+        // dd($this->deliveryReceiveItems);
+
     }
 
     public function render()
--- resources/views/livewire/delivery-receive/create.blade.php
+++ resources/views/livewire/delivery-receive/create.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <form action="">
+    <form wire:submit="store">
         @csrf
         @unless ($purchaseOrders)
             <h1>There are no pending Purchase Orders</h1>
@@ -15,8 +15,108 @@
             @if ($selectedPurchaseOrder)
                 <h1>{{ $selectedPurchaseOrder->id }}</h1>
                 <h1>{{ $selectedPurchaseOrder->supplier->name }}</h1>
-            @endif
-            @endif
 
-        </form>
-    </div>
+                @foreach ($deliveryReceiveItems as $key => $deliveryReceiveItem)
+                    <div class="relative z-0 w-full mb-5 group">
+                        <label for="inventory_item_id"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Inventory
+                            Item</label>
+                        <select wire:model='deliveryReceiveItems.{{ $key }}.inventory_item_id'
+                            wire:change='inventoryItemSelected({{ $key }}, $event.target.value)'
+                            name="inventory_item_id"
+                            id="inventory_item_id{{ $key }}">
+                            <option value=""
+                                selected>-- Please select an Inventory Item --</option>
+                            @foreach ($inventoryItems as $inventoryItem)
+                                <option value="{{ $inventoryItem->id }}">
+                                    {{ $inventoryItem->name }}
+                                    ({{ $inventoryItem->measurement }})
+                                    - {{ $inventoryItem->unit_price }}</option>
+                            @endforeach
+                        </select>
+                        @error("deliveryReceiveItems.$key.inventoryItem")
+                            <span class="text-sm text-red-600 dark:text-red-400 space-y-1">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="relative z-0 w-full mb-5 group">
+                        <label for="expected_quantity_{{ $key }}"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Expected Quantity to be
+                            Received</label>
+                        <input wire:model="deliveryReceiveItems.{{ $key }}.expected_quantity"
+                            wire:change='updateAmount({{ $key }}, $event.target.value)'
+                            wire:keyup='updateAmount({{ $key }}, $event.target.value)'
+                            id="quantity_{{ $key }}"
+                            type="text"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        @error("deliveryReceiveItems.$key.expected_quantity")
+                            <span class="text-sm text-red-600 dark:text-red-400 space-y-1">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="relative z-0 w-full mb-5 group">
+                        <label for="quantity_{{ $key }}"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Received</label>
+                        <input wire:model="deliveryReceiveItems.{{ $key }}.quantity"
+                            wire:change='updateReceived({{ $key }}, $event.target.value)'
+                            wire:keyup='updateReceived({{ $key }}, $event.target.value)'
+                            id="quantity_{{ $key }}"
+                            type="text"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        @error("deliveryReceiveItems.$key.quantity")
+                            <span class="text-sm text-red-600 dark:text-red-400 space-y-1">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="relative z-0 w-full mb-5 group">
+                        <label for="pending_{{ $key }}"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Pending</label>
+                        <input wire:model="deliveryReceiveItems.{{ $key }}.pending"
+                            id="pending_{{ $key }}"
+                            type="text"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        @error("deliveryReceiveItems.$key.pending")
+                            <span class="text-sm text-red-600 dark:text-red-400 space-y-1">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="relative z-0 w-full mb-5 group">
+                        <label for="amount_{{ $key }}"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Amount</label>
+                        <input wire:model.live="deliveryReceiveItems.{{ $key }}.amount"
+                            id="amount_{{ $key }}"
+                            type="text"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        @error("deliveryReceiveItems.$key.amount")
+                            <span class="text-sm text-red-600 dark:text-red-400 space-y-1">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="relative z-0 w-full mb-5 group">
+                        <label for="description_{{ $key }}"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                        <input wire:model="deliveryReceiveItems.{{ $key }}.description"
+                            id="description_{{ $key }}"
+                            type="text"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        @error("deliveryReceiveItems.$key.description")
+                            <span class="text-sm text-red-600 dark:text-red-400 space-y-1">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                @endforeach
+                <div class="mb-6">
+                    <button type="submit"
+                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Create
+                        Order Receive</button>
+                </div>
+            @endif
+        @endunless
+    </form>
+</div>
