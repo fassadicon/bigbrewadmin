86b909df3870e104ee8426cd69fee3e0f45344c5
fassadicon <fa.sadicon@gmail.com>
2023-12-11 22:14:57 +0800

    po dr changes

--- resources/views/livewire/purchase-order/create.blade.php
+++ resources/views/livewire/purchase-order/create.blade.php
@@ -79,7 +79,8 @@
                                 <input wire:model.live="purchaseOrderItems.{{ $key }}.amount"
                                     id="amount_{{ $key }}"
                                     type="text"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    readonly>
                                 @error("purchaseOrderItems.$key.amount")
                                     <span class="text-sm text-red-600 dark:text-red-400 space-y-1">
                                         {{ $message }}
