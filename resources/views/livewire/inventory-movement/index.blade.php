4e34adbe3ea211cd9cc7830ecb787bd4a16a6db4
FA <fa.sadicon@gmail.com>
2023-12-11 16:56:54 +0800

    role permissions 1.1

--- resources/views/livewire/inventory-movement/index.blade.php
+++ resources/views/livewire/inventory-movement/index.blade.php
@@ -7,13 +7,14 @@
 
     <livewire:supplier.create />
 
-    <button x-data=""
-        x-on:click.prevent="$dispatch('open-modal', 'create-supplier')"
-        type="button"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-        Create Supplier
-    </button>
-
+    @role('Super Admin')
+        <button x-data=""
+            x-on:click.prevent="$dispatch('open-modal', 'create-supplier')"
+            type="button"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+            Create Supplier
+        </button>
+    @endrole
     <button wire:click='generateWastage'
         class="px-3 py-1 bg-red-500 text-white rounded ml-8">Export Wastage</button>
 
