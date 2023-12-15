001c283ed0d976c7056e333cd699e01121dd50a2
Arce Ramos <arrambayec@gmail.com>
2023-12-15 12:59:38 +0800

    receipt and ui changes

--- resources/views/livewire/pos/order-summary.blade.php
+++ resources/views/livewire/pos/order-summary.blade.php
@@ -82,92 +82,82 @@
             <div class="px-4 py-6 sm:px-8 sm:py-10">
                 <div class="flow-root">
                     <ul class="-my-8">
-                        @foreach ($selectedProducts as $key => $selectedProduct)
-                        <li class="flex flex-col space-y-3 py-4 text-left sm:flex-row sm:space-x-5 sm:space-y-0">
-                            <div class="shrink-0 relative">
-                                <span
-                                    class="absolute top-1 left-1 flex h-6 w-6 items-center justify-center rounded-full border bg-white text-sm font-medium text-gray-500 shadow sm:-top-2 sm:-right-2">{{
-                                    $loop->index + 1 }}</span>
-                                <img class="h-24 w-24 max-w-full rounded-lg object-cover"
-                                    src="{{ $selectedProduct['product']->productDetail->image }}"
-                                    alt="{{ $selectedProduct['product']->productDetail->name }}" />
-                            </div>
-
-                            <div class="flex-1">
-                                <div class="relative flex flex-1 flex-col justify-between">
-                                    <div class="sm:col-gap-5 sm:grid sm:grid-cols-2">
-                                        <div class="pr-8 sm:pr-5">
-                                            <p class="text-sm font-semibold text-gray-900">
-                                                {{ $selectedProduct['product']->productDetail->name }}
-                                            </p>
-                                            <p class="mx-0 mt-1 mb-0 text-xs text-gray-400">
-                                                {{ $selectedProduct['product']->productDetail->description }}
-                                            </p>
-                                            <p class="text-xs font-semibold text-gray-400">Size:
-                                                {{ $selectedProduct['product']->size->name }}
-                                            </p>
-                                            <p class="text-xs font-semibold text-gray-400">
-                                                ₱{{ $selectedProduct['product']->price }}
-                                            </p>
-                                        </div>
+                        <div class="flex flex-col items-center space-y-4">
+                            @foreach ($selectedProducts as $key => $selectedProduct)
+                                <li class="flex items-stretch space-x-4 py-3 text-left border-b border-gray-300">
+                                    <!-- Product Information Container -->
+                                    <div class="flex-1">
+                                        <p class="text-sm font-semibold text-gray-900">
+                                            {{ $selectedProduct['product']->productDetail->name }}
+                                        </p>
+                                        <p class="text-xs text-gray-400">
+                                            {{ $selectedProduct['product']->productDetail->description }}
+                                        </p>
+                                        <p class="text-xs font-semibold text-gray-400">Size:
+                                            {{ $selectedProduct['product']->size->name }}
+                                        </p>
+                                        <p class="text-xs font-semibold text-gray-400">
+                                            ₱{{ $selectedProduct['product']->price }}
+                                        </p>
                                     </div>
-
-                                    <div class="mt-4 grid grid-cols-3 gap-4">
-                                        <div class="flex items-center">
-                                            <button wire:click='subtractQuantity({{ $key }})'
-                                                class="bg-amber-600 hover:bg-amber-800 text-white text-sm font-bold py-2 px-4 h-6 w-6 rounded">-</button>
-
-                                            <input wire:model='selectedProducts.{{ $key }}.quantity'
-                                                wire:change='updateQuantity' wire:keyup='updateQuantity' type="text">
-                                            {{-- <p class="text-base px-6">{{ $selectedProduct['quantity'] }}</p> --}}
-
-                                            <button wire:click='addQuantity({{ $key }})'
-                                                class="bg-amber-600 hover:bg-amber-800 text-white text-sm font-bold py-2 px-4 h-6 w-6 rounded">+</button>
-                                        </div>
-
+                        
+                                    <!-- Add/Remove Quantity Container -->
+                                    <div class="flex items-center">
+                                        <button wire:click='subtractQuantity({{ $key }})'
+                                            class="bg-amber-600 hover:bg-amber-800 text-white text-sm font-bold py-1 px-2 h-5 w-5 rounded flex items-center justify-center">
+                                            <i class="fas fa-minus"></i>
+                                        </button>
+                        
+                                        <input wire:model='selectedProducts.{{ $key }}.quantity'
+                                            wire:change='updateQuantity' wire:keyup='updateQuantity' type="text"
+                                            class="text-sm px-2 py-1 w-8 border rounded-md">
+                        
+                                        <button wire:click='addQuantity({{ $key }})'
+                                            class="bg-amber-600 hover:bg-amber-800 text-white text-sm font-bold py-1 px-2 h-5 w-5 rounded flex items-center justify-center">
+                                            <i class="fas fa-plus"></i>
+                                        </button>
                                     </div>
-
-                                    <div class="mt-4">
-                                        <div class="mt-6 flex items-center justify-between">
-                                            @php
-                                            $sizeSugarLevels = \App\Models\SizeSugarLevel::where('size_id',
-                                            $selectedProduct['product']->size->id)
-                                            ->orderByDesc('id')
-                                            ->get();
-                                            @endphp
-                                            @if ($sizeSugarLevels->count() > 0)
-                                            <p class="text-sm font-medium text-gray-900">Sugar Level</p>
-                                            <select wire:model.live="selectedProducts.{{ $key }}.sugarLevelId"
-                                                name="sugarLevelId" id="sugarLevelId_{{ $key }}"
-                                                class="block w-32 p-2 border rounded-md">
-                                                {{-- <option value="">No sugar level available</option> --}}
+                        
+                                    <!-- Sugar Level Container -->
+                                    <div class="flex items-center">
+                                        @php
+                                            $sizeSugarLevels = \App\Models\SizeSugarLevel::where('size_id', $selectedProduct['product']->size->id)
+                                                ->orderByDesc('id')
+                                                ->get();
+                                        @endphp
+                                        @if ($sizeSugarLevels->count() > 0)
+                                            <p class="text-sm font-medium text-gray-900 mr-2">Sugar Level</p>
+                                            <select wire:model.live="selectedProducts.{{ $key }}.sugarLevelId" name="sugarLevelId"
+                                                id="sugarLevelId_{{ $key }}" class="block w-16 p-1 border rounded-md text-sm">
                                                 @foreach ($sizeSugarLevels as $sizeSugarLevel)
-                                                <option value="{{ $sizeSugarLevel->id }}" @selected($sizeSugarLevel->id
-                                                    == $selectedProduct['sugarLevelId'])>
-                                                    {{ $sizeSugarLevel->sugarlevel->percentage }}
-                                                </option>
+                                                    <option value="{{ $sizeSugarLevel->id }}"
+                                                        @selected($sizeSugarLevel->id == $selectedProduct['sugarLevelId'])>
+                                                        {{ $sizeSugarLevel->sugarlevel->percentage }}
+                                                    </option>
                                                 @endforeach
                                             </select>
-                                            @endif
-
-                                        </div>
+                                        @endif
                                     </div>
-                                </div>
-                            </div>
+                        
+                                    <!-- Remove Item Container -->
+                                    <div class="ml-auto mt-auto">
+                                        <button wire:click="removeItem({{ $loop->index }})" type="button"
+                                            class="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
+                                            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"
+                                                    class=""></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </div>
+                        
+                        
+                    
 
 
-                            <div class="ml-auto">
-                                <button wire:click="removeItem({{ $loop->index }})" type="button"
-                                    class="flex rounded p-2 text-center text-gray-500 transition-all duration-200 ease-in-out focus:shadow hover:text-gray-900">
-                                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                        viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M6 18L18 6M6 6l12 12" class=""></path>
-                                    </svg>
-                                </button>
-                            </div>
-                        </li>
-                        @endforeach
                     </ul>
                 </div>
 
@@ -193,17 +183,18 @@
                         disabled readonly> --}}
                 </div>
 
-                {{-- Pa blur if walang laman cart --}}
                 <div class="mt-6 text-center">
                     <button wire:click='placeOrder' type="button"
-                        class="group inline-flex w-full h-10 items-center justify-center rounded-md bg-amber-700 px-6 py-4 text-lg font-semibold text-white transition-all duration-200 ease-in-out focus:shadow hover:bg-amber-50 hover:text-amber-950"
+                        class="group inline-flex w-full h-8 items-center justify-center rounded-md bg-amber-700 px-4 py-2 text-sm font-semibold text-white transition-all duration-200 ease-in-out
+                        @if(empty($selectedProducts)) opacity-50 cursor-not-allowed @endif"
                         @disabled(empty($selectedProducts))>
                         Place Order
-                        <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-8 ml-4 h-5 w-5 transition-all"
-                            fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="group-hover:ml-6 ml-2 h-4 w-4 transition-all" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13 7l5 5m0 0l-5 5m5-5H6" />
                         </svg>
                     </button>
+
                 </div>
             </div>
         </div>
--- resources/views/livewire/pos/product-card.blade.php
+++ resources/views/livewire/pos/product-card.blade.php
@@ -13,54 +13,54 @@
     
     <div class="grid grid-cols-2 md:grid-cols-4 gap-4 -mx-2">
         @foreach ($productDetails as $productDetail)
-            <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 px-2 py-2 mb-4 lg:mb-0 flex">
-                <div class="bg-white rounded-lg p-2 transform hover:translate-y-1 hover:shadow-xl transition duration-300 shadow-xl flex flex-col flex-1">
-                    <figure class="mb-2">
-                        <img src="{{ asset('storage\\' . $productDetail->image_path) }}"
-                            alt=""
-                            class="h-16 md:h-24 lg:h-32 ml-auto mr-auto object-cover" />
-                    </figure>
-                    <div class="rounded-lg p-2 bg-zinc-800 flex flex-col flex-1">
-                        <div>
-                            <h5 class="text-white text-xs md:text-sm lg:text-base font-bold leading-tight">
-                                {{ $productDetail->name }}
-                            </h5>
-                            <span class="text-xs md:text-sm text-gray-100 leading-tight">
-                                {{ $productDetail->description }}
-                            </span>
-                        </div>
-                        <div class="flex items-center mt-2">
-                            <div class="text-xs md:text-sm text-white font-light">
-                                <div class="flex space-x-2">
-                                    @foreach ($productDetail->sizes as $size)
-                                        @php
-                                            $disabled = false;
-                                            $warningValue = false;
-                                            foreach ($size->pivot->inventoryItems as $inventoryItem) {
-                                                if ($inventoryItem->remaining_stocks <= $inventoryItem->warning_value) {
-                                                    $warningValue = true;
-                                                }
-                                                if ($inventoryItem->remaining_stocks <= 0) {
-                                                    $disabled = true;
-                                                }
-                                            }
-                                        @endphp
-                                        <button wire:click="addToCart({{ $size->pivot->id }})"
-                                            class="rounded-full h-8 w-8 bg-red-50 text-amber-950 hover:bg-amber-50 hover:text-amber-800 hover:shadow-xl focus:outline-none flex ml-auto transition duration-300"
-                                            @disabled($disabled)>
-                                            <div class="m-auto">
-                                                {{ $size->alias }}
-                                            </div>
-                                        </button>
-                                        ₱{{ $size->pivot->price }}
-                                    @endforeach
-                                </div>
-                            </div>
+    <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 px-2 py-2 mb-4 lg:mb-0 flex">
+        <div class="bg-white rounded-lg p-2 transform hover:translate-y-1 hover:shadow-xl transition duration-300 shadow-xl flex flex-col flex-1">
+            <figure class="mb-2">
+                <img src="{{ asset('storage\\' . $productDetail->image_path) }}"
+                    alt=""
+                    class="h-16 md:h-24 lg:h-32 ml-auto mr-auto object-cover" />
+            </figure>
+            <div class="rounded-lg p-2 bg-zinc-800 flex flex-col flex-1">
+                <div>
+                    <h5 class="text-white text-xxs md:text-xs lg:text-sm font-bold leading-tight">
+                        {{ $productDetail->name }}
+                    </h5>
+                    <span class="text-xxs md:text-xs text-gray-100 leading-tight">
+                        {{ $productDetail->description }}
+                    </span>
+                </div>
+                <div class="flex items-center mt-2">
+                    <div class="text-xxs md:text-xs text-white font-light">
+                        <div class="flex space-x-2">
+                            @foreach ($productDetail->sizes as $size)
+                                @php
+                                    $disabled = false;
+                                    $warningValue = false;
+                                    foreach ($size->pivot->inventoryItems as $inventoryItem) {
+                                        if ($inventoryItem->remaining_stocks <= $inventoryItem->warning_value) {
+                                            $warningValue = true;
+                                        }
+                                        if ($inventoryItem->remaining_stocks <= 0) {
+                                            $disabled = true;
+                                        }
+                                    }
+                                @endphp
+                                <button wire:click="addToCart({{ $size->pivot->id }})"
+                                    class="rounded-full h-8 w-8 bg-red-50 text-amber-950 hover:bg-amber-50 hover:text-amber-800 hover:shadow-xl focus:outline-none flex ml-auto transition duration-300"
+                                    @disabled($disabled)>
+                                    <div class="m-auto">
+                                        {{ $size->alias }}
+                                    </div>
+                                </button>
+                                ₱{{ $size->pivot->price }}
+                            @endforeach
                         </div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        </div>
+    </div>
+@endforeach
 
 
     {{-- <div x-data="{ showModal: @entangle('showModal') }">
