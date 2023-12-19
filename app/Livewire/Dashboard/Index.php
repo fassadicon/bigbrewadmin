5771bf984e4d4ab854a6d4022d0c48ecad6288b8
FA <fa.sadicon@gmail.com>
2023-12-19 10:49:56 +0800

    additionals in dashboard 1.1

--- app/Livewire/Dashboard/Index.php
+++ app/Livewire/Dashboard/Index.php
@@ -2,14 +2,15 @@
 
 namespace App\Livewire\Dashboard;
 
-use App\Models\DeliveryReceive;
-use App\Models\InventoryItem;
+use Carbon\Carbon;
 use App\Models\Order;
 use App\Models\Product;
-use App\Models\PurchaseOrder;
 use Livewire\Component;
+use App\Models\InventoryLog;
+use App\Models\InventoryItem;
+use App\Models\PurchaseOrder;
+use App\Models\DeliveryReceive;
 use Asantibanez\LivewireCharts\Models\ColumnChartModel;
-use Carbon\Carbon;
 
 class Index extends Component
 {
@@ -18,12 +19,12 @@ class Index extends Component
         $today = Carbon::now();
         $currentSalesToday = Order::whereDate('created_at', $today)->sum('total_amount');
 
-        $startOfWeek = $today->startOfWeek();
-        $endOfWeek = $today->endOfWeek();
+        $startOfWeek = Carbon::now()->startOfWeek();
+        $endOfWeek = Carbon::now()->endOfWeek();
         $currentSalesWeek = Order::whereBetween('created_at', [$startOfWeek, $endOfWeek])->sum('total_amount');
 
-        $startOfMonth = $today->startOfMonth();
-        $endOfMonth = $today->endOfMonth();
+        $startOfMonth = Carbon::now()->startOfMonth();
+        $endOfMonth = Carbon::now()->endOfMonth();
 
         $currentSalesMonth = Order::whereBetween('created_at', [$startOfMonth, $endOfMonth])->sum('total_amount');
 
@@ -44,31 +45,41 @@ class Index extends Component
         $purchaseOrders = PurchaseOrder::get();
         $pendingPurchaseOrdersAmount = $purchaseOrders->where('status', 'Pending')->sum('total_amount');
         $purchaseOrdersAmount = $purchaseOrders->sum('total_amount');
+
         $deliveryReceivesAmount = DeliveryReceive::sum('total_amount');
+        $POminusDR = $purchaseOrdersAmount - $deliveryReceivesAmount;
 
         $mostOrderedProducts = Product::withCount('orderItems')
             ->orderByDesc('order_items_count')
             ->limit(5)
             ->get();
 
+        $inventoryLogs = InventoryLog::with('inventoryItem')
+            ->where('type', 3)
+            ->get();
+        $totalAmountWastage = $inventoryLogs->sum('amount');
+        $wasteItems = [];
+        foreach ($inventoryLogs as $inventoryLog) {
+            $wasteItems[] = $inventoryLog->inventoryItem->name;
+        }
+        $wasteItemsCount = count(array_unique($wasteItems));
+
         $columnChartModel =
             (new ColumnChartModel())
             ->setTitle('Sales');
-
-        $currentDayOfWeek = Carbon::now()->dayOfWeek;
+        $currentDayOfWeek = Carbon::now()->startOfWeek();
         foreach (range(1, 7) as $offset) {
-            // Calculate the day of the week for the current iteration
-            $dayOfWeek = ($currentDayOfWeek + 7 - $offset) % 7 + 1;
+            $dayOfWeek = $currentDayOfWeek;
 
-            // Get the sales data for the current day of the week
-            $salesData = Order::whereDate('created_at', Carbon::now()->startOfWeek()->addDays($dayOfWeek - 1))->sum('total_amount');
+            $salesData = Order::whereDate('created_at', $dayOfWeek)->sum('total_amount');
 
-            // Add the column to the chart model
             $columnChartModel->addColumn(
-                Carbon::now()->startOfWeek()->addDays($dayOfWeek - 1)->format('D'),
+                $dayOfWeek->format('D'),
                 $salesData,
                 '#f6ad55'
             );
+
+            $dayOfWeek->addDay();
         }
 
         return view('livewire.dashboard.index', [
@@ -81,7 +92,10 @@ class Index extends Component
             'purchaseOrdersAmount' => $purchaseOrdersAmount,
             'pendingPurchaseOrdersAmount' => $pendingPurchaseOrdersAmount,
             'deliveryReceivesAmount' => $deliveryReceivesAmount,
+            'POminusDR' => $POminusDR,
             'mostOrderedProducts' => $mostOrderedProducts,
+            'wasteItemsCount' => $wasteItemsCount,
+            'totalAmountWastage' => $totalAmountWastage,
         ]);
     }
 }
